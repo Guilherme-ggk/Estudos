aaec543312c2a3e4ac069a0a71fcfc80707eedc6
Gui-gt <ggk0830755@gmail.com>
2024-09-17 09:56:46 -0300

    sobre region e comentarios

--- Primeiro/ConsoleC#/Program.cs
+++ Primeiro/ConsoleC#/Program.cs
@@ -10,6 +10,23 @@ namespace ConsoleC_
     {
         static void Main(string[] args)
         {
+            //Comentários 
+            //são feitos para organizar o código, documentar o que
+            //cada parte faz para ajudar possiveis outros programadores de equipe
+
+            //Comentario
+
+            //#Region
+            //Feito tbm para organizar os blocos de código
+            //documentar e deixar nomes para cada bloco
+
+            #region Executa um linha de comando
+
+            Console.WriteLine("Hello Word!");
+
+            #endregion
+
+
         }
     }
 }

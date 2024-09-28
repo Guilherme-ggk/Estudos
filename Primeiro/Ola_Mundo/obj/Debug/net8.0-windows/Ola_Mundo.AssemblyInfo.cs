2ae42fa92ba945d8af12601263ae9e8976629d6b
Gui-gt <ggk0830755@gmail.com>
2024-09-28 11:27:58 -0300

    codigo de um caracter

--- Primeiro/Ola_Mundo/obj/Debug/net8.0-windows/Ola_Mundo.AssemblyInfo.cs
+++ Primeiro/Ola_Mundo/obj/Debug/net8.0-windows/Ola_Mundo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ola_Mundo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bc45451ba47374601122920fd34309f2814c6ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df57d49cf6cbbbdd094f4a7478451698cbce21cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ola_Mundo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ola_Mundo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

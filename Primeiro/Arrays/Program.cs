912e4fbdce701aaee8731b1bfe01466d90511db1
Gui-gt <ggk0830755@gmail.com>
2024-10-26 13:46:36 -0300

    ATT

--- Primeiro/Arrays/Program.cs
+++ Primeiro/Arrays/Program.cs
@@ -10,7 +10,43 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            
+            //Vetores (array)
+
+            int[] numeros = { 1, 2, 3 };
+
+            string[] carros =
+            {
+                "lamborghini",
+                "mitsubishi",
+                "volkswagen"
+            };
+
+            //matriz (array bimedimensional)
+
+            int[,] numeros2 = new int[2,3];
+
+            numeros2[0, 0] = 1;
+            numeros2[0, 1] = 2;
+            numeros2[0, 2] = 3;
+    
+
+            numeros2[1, 0] = 10;
+            numeros2[1, 1] = 20;
+            numeros2[1, 2] = 30;
+
+            Console.Write("[" + numeros2[0, 0] + "]");
+            Console.Write("[" + numeros2[0, 1] + "]");
+            Console.Write("[" + numeros2[0, 2] + "]");
+
+            Console.WriteLine();
+
+            Console.Write("[" + numeros2[1, 0] + "]");
+            Console.Write("[" + numeros2[1, 1] + "]");
+            Console.Write("[" + numeros2[1, 2] + "]");
+
+            Console.ReadLine();
+
+
 
         }
     }

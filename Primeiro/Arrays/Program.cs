b10bde291d2af604ddb26b2ae08e6ef2814b66f6
Gui-gt <ggk0830755@gmail.com>
2024-10-26 12:06:09 -0300

    att

--- Primeiro/Arrays/Program.cs
+++ Primeiro/Arrays/Program.cs
@@ -10,18 +10,7 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            double c, f, k;
-            Console.WriteLine("Conversor de temperatura");
-            Console.WriteLine("Digite a temperatura que deseja converter em graus celcius:");
-            c = double.Parse(Console.ReadLine());
-
-            f = (c * 9 / 5) + 32;
-
-            k = (c + 273.15);
-
-            Console.WriteLine(c + " graus celcius = " + f + " graus fahrenheit");
-            Console.WriteLine(c + " graus celcius = " + k + " graus kelvin");
-            Console.ReadKey();
+            
 
         }
     }

eac39cf99eac993e69715b03657502249b4c9f6e
Gui-gt <ggk0830755@gmail.com>
2024-11-10 15:00:16 -0300

    att

--- SistemaComercio/CamadaDados/DCategoria.cs
+++ SistemaComercio/CamadaDados/DCategoria.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace CamadaDados
 {
     internal class DCategoria
     {
+        private int _Idcategoria;
+        private string _Nome;
+        private string _Descricao;
+        private string _Textobuscar;
+
+        public int Idcategoria { get => _Idcategoria; set => _Idcategoria = value; }
+        public string Nome { get => _Nome; set => _Nome = value; }
+        public string Descricao { get => _Descricao; set => _Descricao = value; }
+        public string Textobuscar { get => _Textobuscar; set => _Textobuscar = value; }
+
     }
 }

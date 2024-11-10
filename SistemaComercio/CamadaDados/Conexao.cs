9abbdd466fc52cf090b8ef517c6a42fbac11595e
Gui-gt <ggk0830755@gmail.com>
2024-11-10 15:47:36 -0300

    Csharp

--- SistemaComercio/CamadaDados/Conexao.cs
+++ SistemaComercio/CamadaDados/Conexao.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace CamadaDados
 {
     internal class Conexao
-    {
+    {                                               //tirar essa barra depois e deixar so o PCGUI
+        public static string Cn = "Data Source = PCGUI\\MSSQLSERVER2014; Initial Catalog = dbcomercio; Integrated Security = true";
     }
 }

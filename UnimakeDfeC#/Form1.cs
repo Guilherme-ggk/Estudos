9592cda5d1c058332a094f9b5306c298daf08a1f
Gui-gt <ggk0830755@gmail.com>
2024-11-19 10:27:29 -0300

    att

--- UnimakeDfeC#/Form1.cs
+++ UnimakeDfeC#/Form1.cs
@@ -8,7 +8,13 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Unimake.Business.DFe.Servicos;
+using Unimake.Business.DFe.Servicos.NFe;
+using Unimake.Business.DFe.Xml.GNRE;
+using Unimake.Business.DFe.Xml.NF3e;
+using Unimake.Business.DFe.Xml.NFe;
 using Unimake.Business.Security;
+using ConsStatServ = Unimake.Business.DFe.Xml.NFe.ConsStatServ;
 
 
 namespace UnimakeDfeC_
@@ -22,21 +28,60 @@ namespace UnimakeDfeC_
 
         private void BtnConsultaStatus_Click(object sender, EventArgs e)
         {
+            var xml = new ConsStatServ  //Montando XML
+            {
+                Versao = "4.00", // versão
+                CUF = UFBrasil.PR, //localização
+                TpAmb = TipoAmbiente.Homologacao //Tipo ambiente
+            };
+            //primeiro ele controi o obrjeto xml, contori a config, com certificado, servico, tipo de emissao e dfe, com isso depois ele puxa mais detalhes, 
+            //depois executa e consome o servico, status mostra todos os detalhes do retorno da sefaz
 
+            var configuracao = new Configuracao
+            {
+                TipoDFe = TipoDFe.NFe,
+                TipoEmissao = TipoEmissao.Normal,
+                CertificadoDigital = CertificadoSelecionado,
+
+            };
+
+            var StatusServico = new StatusServico(xml, configuracao);
+            StatusServico.Executar();
+
+            MessageBox.Show(StatusServico.Result.CStat + " " + StatusServico.Result.XMotivo);
         }
 
         private void BtnConsultaSituacao_Click(object sender, EventArgs e)
         {
+            var xml = new ConsSitNFe
+            {
+                Versao = "4.00",
+                TpAmb = TipoAmbiente.Homologacao,
+                ChNFe = ""
+            };
+
+            var configuracao = new Configuracao
+            {
+                TipoDFe = TipoDFe.NFe,
+                TipoEmissao = TipoEmissao.Normal,
+                CertificadoDigital = CertificadoSelecionado
+            };
+
+            var consultaProtocolo = new ConsultaProtocolo(xml, configuracao);
+            consultaProtocolo.Executar();
+
+            MessageBox.Show(consultaProtocolo.Result.CStat + " " + consultaProtocolo.Result.XMotivo);
+
 
         }
 
         #region Certificado digital
 
         //Caminho do certificado digital A1
-        private static string PathCertificadoDigital { get; set; } = @"C:\Unimake\UnimakePV.pfx";
+        private static string PathCertificadoDigital { get; set; } = @"C:\Users\30755\Downloads\file.p12";
 
         //Senha do certificado digital 
-        private static string SenhaCertificadoDigital { get; set; } = "123456789";
+        private static string SenhaCertificadoDigital { get; set; } = "30163001";
 
         private static X509Certificate2 CertificadoSelecionadoField; // Todo certificado digital alimenta essa propriedade
         
@@ -58,7 +103,7 @@ namespace UnimakeDfeC_
         }
 
 
-
+        
         #endregion
     }
 }

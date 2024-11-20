e7005ebc68fce18c3a4d69c959c8e28d0779fccb
Gui-gt <ggk0830755@gmail.com>
2024-11-20 17:58:18 -0300

    att

--- UnimakeDfeC#/Form1.cs
+++ UnimakeDfeC#/Form1.cs
@@ -28,18 +28,55 @@ namespace UnimakeDfeC_
 
         private void BtnConsultaStatus_Click(object sender, EventArgs e)
         {
+            var xml = new ConsStatServ
+            {
+                Versao = "4.00",
+                CUF = UFBrasil.PR,
+                TpAmb = TipoAmbiente.Homologacao
+            };
 
+            var configuracao = new Configuracao
+            {
+                CertificadoDigital = CertificadoSelecionado,
+                TipoDFe = TipoDFe.NFe,
+                TipoEmissao = TipoEmissao.Normal
+            };
+
+            var StatusServico = new StatusServico(xml, configuracao);
+            StatusServico.Executar();
+
+            MessageBox.Show(StatusServico.Result.CStat + " " + StatusServico.Result.XMotivo);
+
+                
         }
 
         private void BtnConsultaSituacao_Click(object sender, EventArgs e)
         {
 
+            var xml = new ConsSitNFe
+            {
+                Versao = "4.00",
+                TpAmb = TipoAmbiente.Homologacao,
+                ChNFe = "41241135547526000170550010000019261000019274"
+            };
+
+            var configuracao = new Configuracao
+            {
+                CertificadoDigital = CertificadoSelecionado,
+                TipoDFe = TipoDFe.NFe,
+                TipoEmissao = TipoEmissao.Normal
+            };
+
+            var ConsultaProtocolo = new ConsultaProtocolo(xml, configuracao);
+            ConsultaProtocolo.Executar();
+
+            MessageBox.Show(ConsultaProtocolo.Result.CStat + " " + ConsultaProtocolo.Result.XMotivo);
         }
 
         #region
 
-        private static string PathCertificadoDigital { get; set; } = "";
-        private static string SenhaCertificado { get; set; } = "";
+        private static string PathCertificadoDigital { get; set; } = "C:\\Users\\30755\\Downloads\\HS BERNARDO - COMERCIO DE CARNES LTDA Senha 30163001 Val 19.11.2025 1006927767.pfx";
+        private static string SenhaCertificado { get; set; } = "30163001";
 
         public static X509Certificate2 CertificadoDigitalField;
 
@@ -53,10 +90,8 @@ namespace UnimakeDfeC_
                 }
                 return CertificadoDigitalField;
             }
-
             private set => throw new Exception("não é possivel carregar certificado digital, carregado automaticamente!");
         }
         #endregion
-
     }
 }

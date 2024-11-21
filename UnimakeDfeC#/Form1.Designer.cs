8fac030324724e16f96e78e54465ca4e92ec6061
Gui-gt <ggk0830755@gmail.com>
2024-11-20 21:44:58 -0300

    att

--- UnimakeDfeC#/Form1.Designer.cs
+++ UnimakeDfeC#/Form1.Designer.cs
@@ -29,32 +29,32 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.BtnConsultaStatus = new System.Windows.Forms.Button();
             this.BtnConsultaSituacao = new System.Windows.Forms.Button();
+            this.BtnConsultaStatus = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.BtnEnviarNFeSincrono = new System.Windows.Forms.Button();
+            this.BtnEnviarNFeAssincrono = new System.Windows.Forms.Button();
+            this.BtnEnviarNFeAssincronoLote = new System.Windows.Forms.Button();
+            this.BtnInutilizacao = new System.Windows.Forms.Button();
+            this.BtnConsultaCadastroContribuinte = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.BtnConsultaCadastroContribuinte);
+            this.groupBox1.Controls.Add(this.BtnInutilizacao);
+            this.groupBox1.Controls.Add(this.BtnEnviarNFeAssincronoLote);
+            this.groupBox1.Controls.Add(this.BtnEnviarNFeAssincrono);
+            this.groupBox1.Controls.Add(this.BtnEnviarNFeSincrono);
             this.groupBox1.Controls.Add(this.BtnConsultaSituacao);
             this.groupBox1.Controls.Add(this.BtnConsultaStatus);
             this.groupBox1.Location = new System.Drawing.Point(5, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(248, 545);
+            this.groupBox1.Size = new System.Drawing.Size(248, 620);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "NFe";
-            // 
-            // BtnConsultaStatus
-            // 
-            this.BtnConsultaStatus.Location = new System.Drawing.Point(3, 35);
-            this.BtnConsultaStatus.Name = "BtnConsultaStatus";
-            this.BtnConsultaStatus.Size = new System.Drawing.Size(241, 38);
-            this.BtnConsultaStatus.TabIndex = 0;
-            this.BtnConsultaStatus.Text = "Consulta Status";
-            this.BtnConsultaStatus.UseVisualStyleBackColor = true;
-            this.BtnConsultaStatus.Click += new System.EventHandler(this.BtnConsultaStatus_Click);
             // 
             // BtnConsultaSituacao
             // 
@@ -66,11 +66,66 @@
             this.BtnConsultaSituacao.UseVisualStyleBackColor = true;
             this.BtnConsultaSituacao.Click += new System.EventHandler(this.BtnConsultaSituacao_Click);
             // 
+            // BtnConsultaStatus
+            // 
+            this.BtnConsultaStatus.Location = new System.Drawing.Point(3, 35);
+            this.BtnConsultaStatus.Name = "BtnConsultaStatus";
+            this.BtnConsultaStatus.Size = new System.Drawing.Size(241, 38);
+            this.BtnConsultaStatus.TabIndex = 0;
+            this.BtnConsultaStatus.Text = "Consulta Status";
+            this.BtnConsultaStatus.UseVisualStyleBackColor = true;
+            this.BtnConsultaStatus.Click += new System.EventHandler(this.BtnConsultaStatus_Click);
+            // 
+            // BtnEnviarNFeSincrono
+            // 
+            this.BtnEnviarNFeSincrono.Location = new System.Drawing.Point(3, 214);
+            this.BtnEnviarNFeSincrono.Name = "BtnEnviarNFeSincrono";
+            this.BtnEnviarNFeSincrono.Size = new System.Drawing.Size(241, 38);
+            this.BtnEnviarNFeSincrono.TabIndex = 2;
+            this.BtnEnviarNFeSincrono.Text = "Enviar NFe Síncrono";
+            this.BtnEnviarNFeSincrono.UseVisualStyleBackColor = true;
+            // 
+            // BtnEnviarNFeAssincrono
+            // 
+            this.BtnEnviarNFeAssincrono.Location = new System.Drawing.Point(4, 278);
+            this.BtnEnviarNFeAssincrono.Name = "BtnEnviarNFeAssincrono";
+            this.BtnEnviarNFeAssincrono.Size = new System.Drawing.Size(241, 38);
+            this.BtnEnviarNFeAssincrono.TabIndex = 3;
+            this.BtnEnviarNFeAssincrono.Text = "Enviar NFe Assíncrono";
+            this.BtnEnviarNFeAssincrono.UseVisualStyleBackColor = true;
+            // 
+            // BtnEnviarNFeAssincronoLote
+            // 
+            this.BtnEnviarNFeAssincronoLote.Location = new System.Drawing.Point(3, 340);
+            this.BtnEnviarNFeAssincronoLote.Name = "BtnEnviarNFeAssincronoLote";
+            this.BtnEnviarNFeAssincronoLote.Size = new System.Drawing.Size(241, 38);
+            this.BtnEnviarNFeAssincronoLote.TabIndex = 4;
+            this.BtnEnviarNFeAssincronoLote.Text = "Enviar NFe Assíncrono em Lote";
+            this.BtnEnviarNFeAssincronoLote.UseVisualStyleBackColor = true;
+            // 
+            // BtnInutilizacao
+            // 
+            this.BtnInutilizacao.Location = new System.Drawing.Point(4, 488);
+            this.BtnInutilizacao.Name = "BtnInutilizacao";
+            this.BtnInutilizacao.Size = new System.Drawing.Size(241, 38);
+            this.BtnInutilizacao.TabIndex = 5;
+            this.BtnInutilizacao.Text = "Inutilização";
+            this.BtnInutilizacao.UseVisualStyleBackColor = true;
+            // 
+            // BtnConsultaCadastroContribuinte
+            // 
+            this.BtnConsultaCadastroContribuinte.Location = new System.Drawing.Point(4, 561);
+            this.BtnConsultaCadastroContribuinte.Name = "BtnConsultaCadastroContribuinte";
+            this.BtnConsultaCadastroContribuinte.Size = new System.Drawing.Size(241, 38);
+            this.BtnConsultaCadastroContribuinte.TabIndex = 6;
+            this.BtnConsultaCadastroContribuinte.Text = "Consulta Cadastro Contribuinte";
+            this.BtnConsultaCadastroContribuinte.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(1067, 636);
             this.Controls.Add(this.groupBox1);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
@@ -87,6 +142,11 @@
         private System.Windows.Forms.Button BtnConsultaSituacao;
         private System.Windows.Forms.Button BtnConsultaStatus;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button BtnConsultaCadastroContribuinte;
+        private System.Windows.Forms.Button BtnInutilizacao;
+        private System.Windows.Forms.Button BtnEnviarNFeAssincronoLote;
+        private System.Windows.Forms.Button BtnEnviarNFeAssincrono;
+        private System.Windows.Forms.Button BtnEnviarNFeSincrono;
     }
 }
 

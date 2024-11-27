6a8f938050559899ffc573f1c2144427e3d9e01a
Gui-gt <ggk0830755@gmail.com>
2024-11-27 18:40:45 -0300

    att

---
+++ POO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aprendendo sobre POO
+
+            MinhaClasse Objeto1 = new MinhaClasse();
+            MinhaClasse Objeto2 = Objeto1;
+
+        }
+
+
+        class MinhaClasse
+        {
+            
+        }
+
+    }
+}

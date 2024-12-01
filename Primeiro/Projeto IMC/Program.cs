58c61c2026a1ce8fac2c42162f5cc7fb0ed9a320
Gui-gt <ggk0830755@gmail.com>
2024-12-01 07:40:07 -0300

    att

--- Primeiro/Projeto IMC/Program.cs
+++ Primeiro/Projeto IMC/Program.cs
@@ -11,9 +11,34 @@ namespace Projeto_IMC
         enum Menu { soma = 1, sub, mult, div, pot, raiz, sair };
         static void Main(string[] args)
         {
-            Console.WriteLine("Calculuadora no Console");
-            Console.WriteLine("1-soma\n2-subtração\n3-multplicação\n4-divisão\n5-potência\n6-raiz\n7-sair");
-            int intop = 
+            bool EscolheuSair = false;
+            while (!EscolheuSair)
+            {
+                Console.WriteLine("Calculuadora no Console");
+                Console.WriteLine("1-soma\n2-subtração\n3-multplicação\n4-divisão\n5-potência\n6-raiz\n7-sair");
+                int intop = int.Parse(Console.ReadLine());
+                Menu opcao = (Menu)intop;
+
+                switch (opcao)
+                {
+                    case Menu.soma:
+                        break;
+                    case Menu.sub:
+                        break;
+                    case Menu.mult:
+                        break;
+                    case Menu.div:
+                        break;
+                    case Menu.pot:
+                        break;
+                    case Menu.raiz:
+                        break;
+                    case Menu.sair:
+                        EscolheuSair = true;
+                        break;
+                }
+            }
+           
         }
     
     }

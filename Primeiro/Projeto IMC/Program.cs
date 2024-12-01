e92add2b23565de2b5497aa0e013567924af9afb
Gui-gt <ggk0830755@gmail.com>
2024-12-01 07:36:49 -0300

    Calculadora no console

--- Primeiro/Projeto IMC/Program.cs
+++ Primeiro/Projeto IMC/Program.cs
@@ -11,7 +11,9 @@ namespace Projeto_IMC
         enum Menu { soma = 1, sub, mult, div, pot, raiz, sair };
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Calculuadora no Console");
+            Console.WriteLine("1-soma\n2-subtração\n3-multplicação\n4-divisão\n5-potência\n6-raiz\n7-sair");
+            int intop = 
         }
     
     }

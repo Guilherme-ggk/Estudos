b20615e36bad9788a65b40a985b4d6c06621948b
Gui-gt <ggk0830755@gmail.com>
2024-12-01 07:59:09 -0300

    att

--- Primeiro/Projeto IMC/Program.cs
+++ Primeiro/Projeto IMC/Program.cs
@@ -8,114 +8,22 @@ namespace Projeto_IMC
 {
     internal class Program
     {
-        enum Menu { soma = 1, sub, mult, div, pot, raiz, sair };
         static void Main(string[] args)
         {
-            bool EscolheuSair = false;
-            while (!EscolheuSair)
-            {
-                Console.WriteLine("Calculuadora no Console");
-                Console.WriteLine("1-soma\n2-subtração\n3-multplicação\n4-divisão\n5-potência\n6-raiz\n7-sair");
-                int intop = int.Parse(Console.ReadLine());
-                Menu opcao = (Menu)intop;
+            double c, k, f;
 
-                switch (opcao)
-                {
-                    case Menu.soma:
-                        Soma();
-                        break;
-                    case Menu.sub:
-                        Sub();
-                        break;
-                    case Menu.mult:
-                        Mult();
-                        break;
-                    case Menu.div:
-                        Div();
-                        break;
-                    case Menu.pot:
-                        Pot();
-                        break;
-                    case Menu.raiz:
-                        Raiz();
-                        break;
-                    case Menu.sair:
-                        EscolheuSair = true;
-                        break;
-                }
-                Console.Clear();
-            }
-           
-        }
-        static void Soma()
-        {
-            Console.WriteLine("Digite o primeiro número:");
-            int n1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o segundo número:");
-            int n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Conversor de temperatura no console");
 
-            int resultado = n1 + n2;
+            Console.WriteLine("Digite a temperatura em graus celcius:");
+            c = double.Parse(Console.ReadLine());
 
-            Console.WriteLine(resultado);
-            Console.ReadLine();
-        }
-        static void Sub()
-        {
-            Console.WriteLine("Digite o primeiro número:");
-            int n1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o segundo número:");
-            int n2 = int.Parse(Console.ReadLine());
+            k = 273.15 + c;
 
-            int resultado = n1 - n2;
+            f = (c * 9 / 5) + 32;
 
-            Console.WriteLine(resultado);
-            Console.ReadLine();
-        }
-        static void Mult()
-        {
-            Console.WriteLine("Digite o primeiro número:");
-            int n1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o segundo número:");
-            int n2 = int.Parse(Console.ReadLine());
-
-            int resultado = n1 * n2;
-
-            Console.WriteLine(resultado);
-            Console.ReadLine();
-        }
-        static void Div()
-        {
-            Console.WriteLine("Digite o primeiro número:");
-            int n1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o segundo número:");
-            int n2 = int.Parse(Console.ReadLine());
-
-            float resultado = (float)n1 / (float)n2;
-
-            Console.WriteLine(resultado);
-            Console.ReadLine();
-        }
-        static void Pot()
-        {
-            Console.WriteLine("Digite o número base:");
-            int b1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o número expoente:");
-            int e2 = int.Parse(Console.ReadLine());
-
-            int resultado = (int)Math.Pow(b1, e2);
-
-            Console.WriteLine(resultado);
-            Console.ReadLine();
-        }
-        static void Raiz()
-        {
-            Console.WriteLine("Digite o número que deseja saber a raiz:");
-            int n1 = int.Parse(Console.ReadLine());
-
-            int resultado = (int)Math.Sqrt(n1);
-
-            Console.WriteLine(resultado);
-            Console.ReadLine();
+            Console.WriteLine($"{c} = {k} graus kelvin.");
+            Console.WriteLine($"{c} = {f} graus farenheit.");
+         
         }
 
     }

b1a14c45bf9b4c56566bfa3012ecab6727a39625
Gui-gt <ggk0830755@gmail.com>
2024-12-05 20:10:28 -0300

    att

--- Primeiro/Projeto IMC/Program.cs
+++ Primeiro/Projeto IMC/Program.cs
@@ -10,19 +10,7 @@ namespace Projeto_IMC
     {
         static void Main(string[] args)
         {
-            double c, k, f;
-
-            Console.WriteLine("Conversor de temperatura no console");
-
-            Console.WriteLine("Digite a temperatura em graus celcius:");
-            c = double.Parse(Console.ReadLine());
-
-            k = 273.15 + c;
-
-            f = (c * 9 / 5) + 32;
-
-            Console.WriteLine($"{c} = {k} graus kelvin.");
-            Console.WriteLine($"{c} = {f} graus farenheit.");
+            
          
         }
 

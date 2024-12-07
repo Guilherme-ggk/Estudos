1060fe9597676d0841d05fca9d773255d91e6e20
Gui-gt <ggk0830755@gmail.com>
2024-12-07 09:21:55 -0300

    att

--- Primeiro/Projeto IMC/Program.cs
+++ Primeiro/Projeto IMC/Program.cs
@@ -8,126 +8,22 @@ namespace Projeto_IMC
 {
     internal class Program
     {
-        enum Menu { soma = 1, sub, mult, div, pot, raiz, sair};
-
         static void Main()
         {
-            bool EscolheuSair = false;
-            while(EscolheuSair == false)
-            {
-                Console.WriteLine("Calculadora:");
-                Console.WriteLine("1-soma\n2-subtração\n3-multiplicação\n4-divisão\n5-potência\n6-raiz\n7-sair");
-                int intop = int.Parse(Console.ReadLine());
-                Menu opcao = (Menu)intop;
+            double c, k, f;
+            Console.WriteLine("Conversor de temperatura:");
+            Console.WriteLine("Digite a temperatura em graus celcius:");
+            c = double.Parse(Console.ReadLine());
 
-                switch (opcao)
-                {
-                    case Menu.soma:
-                        Soma();
-                        break;
-                    case Menu.sub:
-                        Sub();
-                        break;
-                    case Menu.mult:
-                        Mult();
-                        break;
-                    case Menu.div:
-                        Div();
-                        break;
-                    case Menu.pot:
-                        Pot();
-                        break;
-                    case Menu.raiz:
-                        Raiz();
-                        break;
-                    case Menu.sair:
-                        EscolheuSair = true;
-                        break;
-                }
-                Console.Clear();
-            }
+            f = (c * 9 / 5) + 32;
+            
+            k = c + 273.15;
+
+            Console.WriteLine($"{c} graus celcius = {f} graus farenheit.");
+
+            Console.WriteLine($"{c} graus celcius = {k} graus kelvin.");
+
+            Console.ReadLine();
         }
-        static void Soma()
-        {
-            Console.WriteLine("Digite o primeiro número:");
-            int n1 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Digite o segundo número:");
-            int n2 = int.Parse(Console.ReadLine());
-
-            int res = n1 + n2;
-
-            Console.WriteLine(res);
-
-            Console.ReadKey();
-        }
-        static void Sub()
-        {
-            Console.WriteLine("Digite o primeiro número:");
-            int n1 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Digite o segundo número:");
-            int n2 = int.Parse(Console.ReadLine());
-
-            int res = n1 - n2;
-
-            Console.WriteLine(res);
-
-            Console.ReadKey();
-        }
-        static void Mult()
-        {
-            Console.WriteLine("Digite o primeiro número:");
-            int n1 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Digite o segundo número:");
-            int n2 = int.Parse(Console.ReadLine());
-
-            int res = n1 * n2;
-
-            Console.WriteLine(res);
-
-            Console.ReadKey();
-        }
-        static void Div()
-        {
-            Console.WriteLine("Digite o primeiro número:");
-            int n1 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Digite o segundo número:");
-            int n2 = int.Parse(Console.ReadLine());
-
-            float res = (float)n1 / (float)n2;
-
-            Console.WriteLine(res);
-
-            Console.ReadKey();
-        }
-        static void Pot()
-        {
-            Console.WriteLine("Digite o número base:");
-            int b1 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Digite o número expoente:");
-            int e2 = int.Parse(Console.ReadLine());
-
-            int res = (int)Math.Pow(b1, e2);
-
-            Console.WriteLine(res);
-
-            Console.ReadKey();
-        }
-        static void Raiz()
-        {
-            Console.WriteLine("Digite o número que deseja saber a raiz:");
-            int n1 = int.Parse(Console.ReadLine());
-                
-            int res = (int)Math.Sqrt(n1);
-
-            Console.WriteLine(res);
-
-            Console.ReadKey();
-        }
-
     }
 }

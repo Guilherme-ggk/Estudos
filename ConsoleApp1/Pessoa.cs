00f143ef72aecfe24111d373f3a1b82fe6ca81c7
Gui-gt <ggk0830755@gmail.com>
2024-12-12 20:29:34 -0300

    att

---
+++ ConsoleApp1/Pessoa.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Pessoa
+    {
+        public string nome;
+        public int anonascimento;
+        public int idade;
+
+        public Pessoa()
+        {
+            nome = "Guilherme";
+            anonascimento = 0;
+            idade = 0;
+        }
+
+        public Pessoa(string nome, int anonascimento)
+        {
+            this.nome = nome;
+            this.anonascimento = anonascimento;
+            idade = Idade();
+        }
+
+
+        private int Idade()
+        {
+            return 2024 - anonascimento;
+        }
+
+        public Pessoa(string nome, int idade)
+        {
+            this.nome = nome;
+            this.anonascimento = anonascimento;
+            idade = Idade();
+        }
+    }
+}

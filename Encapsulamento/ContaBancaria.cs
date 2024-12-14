65ed5c054180f78236b43ebbc699f77884397e97
Gui-gt <ggk0830755@gmail.com>
2024-12-14 12:17:27 -0300

    att

---
+++ Encapsulamento/ContaBancaria.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento
+{
+    internal class ContaBancaria
+    {
+        private string _cliente;
+        private double _saldo;
+        private double _taxa = 10;
+
+        public string Cliente
+        {
+            get
+            {
+                return _cliente; 
+            }
+            set
+            {
+                if(value != "Guilherme" && value != "Giovana")
+                {
+                    _cliente = "Visitante";
+                    _saldo = 0;
+                }
+                else
+                {
+                    _cliente = value;
+                    Saldo = 100;
+                }
+            }
+        }
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            private set
+            {
+                _saldo = value;
+            }
+        }
+
+
+        public void Sacar(double valor)
+        {
+            valor += _taxa;
+            _saldo -= valor;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+    }
+}
---
+++ Encapsulamento/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria c = new ContaBancaria();
+
+            c.Cliente = "Guilherme";
+            //c.Saldo = 100;
+
+            // ================ OPERAÇÃO DEPOSITO ====================
+
+            double valor = 100;
+            c.Depositar(valor);
+
+            // ================ OPERAÇÃO SACAR ====================
+
+            double valor2 = 50;
+            c.Sacar(valor2);
+
+            // ================ CLIENTE ====================
+
+            Console.WriteLine($"Cliente: {c.Cliente}");
+            Console.WriteLine($"Cliente: {c.Saldo}");
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}

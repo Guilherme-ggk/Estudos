1e8a9b3903493a3148dff8c1521fcc0b5ce21bb4
Gui-gt <ggk0830755@gmail.com>
2024-12-15 15:33:30 -0300

    att

--- Primeiro/ConsoleApp2/Program.cs
+++ Primeiro/ConsoleApp2/Program.cs
@@ -8,27 +8,122 @@ namespace ConsoleApp2
 {
     internal class Program
     {
+        enum Menu { soma = 1, sub, mult, div, pot, raiz, sair }
         static void Main(string[] args)
         {
-            Console.WriteLine("Digite o seu peso:");
-            double peso = double.Parse(Console.ReadLine());
+            bool EscolheuSair = false;
+            while(!EscolheuSair)
+            {
+                Console.WriteLine("Calculadora");
+                Console.WriteLine("1-soma\n2-subtração\n3-multiplicação\n4-divisão\n5-potência\n6-raiz\n7-sair");
+                int inop = int.Parse(Console.ReadLine());
+                Menu opcao = (Menu)inop;
 
-            if(peso <= 24)
-            {
-                Console.WriteLine("Magro");
+                switch (opcao)
+                {
+                    case Menu.soma:
+                        Soma();
+                        break;
+                    case Menu.sub:
+                        Sub();
+                        break;
+                    case Menu.mult:
+                        Mult();
+                        break;
+                    case Menu.div:
+                        Div();
+                        break;
+                    case Menu.pot:
+                        Pot();
+                        break;
+                    case Menu.raiz:
+                        Raiz();
+                        break;
+                    case Menu.sair:
+                        EscolheuSair = true;
+                        break;
+                }
+                Console.Clear();
             }
-            else if(peso == 24)
-            {
-                Console.WriteLine("peso ideal");
-            }
-            else if(peso >= 24 && peso <= 29)
-            {
-                Console.WriteLine("Acima do peso");
-            }
-            else
-            {
-                Console.WriteLine("Obeso");
-            }
+        }
+        static void Soma()
+        {
+            Console.WriteLine("Digite o primeiro número:");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            int n2 = int.Parse(Console.ReadLine());
+
+            int resultado = n1 + n2;
+
+            Console.WriteLine(resultado);
+
+            Console.ReadLine();
+        }
+        static void Sub()
+        {
+            Console.WriteLine("Digite o primeiro número:");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            int n2 = int.Parse(Console.ReadLine());
+
+            int resultado = n1 - n2;
+
+            Console.WriteLine(resultado);
+
+            Console.ReadLine();
+        }
+        static void Mult()
+        {
+            Console.WriteLine("Digite o primeiro número:");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            int n2 = int.Parse(Console.ReadLine());
+
+            int resultado = n1 * n2;
+
+            Console.WriteLine(resultado);
+
+            Console.ReadLine();
+        }
+        static void Div()
+        {
+            Console.WriteLine("Digite o primeiro número:");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            int n2 = int.Parse(Console.ReadLine());
+
+            float resultado = (float)n1 / (float)n2;
+
+            Console.WriteLine(resultado);
+
+            Console.ReadLine();
+        }
+        static void Pot()
+        {
+            Console.WriteLine("Digite o número base:");
+            int b1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o número expoente:");
+            int e2 = int.Parse(Console.ReadLine());
+
+            int resultado = (int)Math.Pow(b1, e2);
+
+            Console.WriteLine(resultado);
+
+            Console.ReadLine();
+        }
+        static void Raiz()
+        {
+            Console.WriteLine("Digite o número que deseja saber a raiz:");
+            int n1 = int.Parse(Console.ReadLine());
+
+            int resultado = (int)Math.Sqrt(n1);
+
+            Console.WriteLine(resultado);
 
             Console.ReadLine();
         }

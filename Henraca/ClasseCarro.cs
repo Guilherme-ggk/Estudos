802a6cb9b8b261e4d4aa0a158e370d09a0660f32
Gui-gt <ggk0830755@gmail.com>
2024-12-16 11:34:06 -0300

    att

---
+++ Henraca/ClasseCarro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Henraca
+{
+    internal class ClasseCarro : Veiculo
+    {
+        public int VelocidadeMaxima {get; set;}
+        public void LigarMotor()
+        {
+            Console.WriteLine("Motor ligado");
+        }
+    }
+}

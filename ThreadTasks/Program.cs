a0337ab99b6491877e07332e58429dc1da4a98a1
Gui-gt <ggk0830755@gmail.com>
2025-01-10 19:35:31 -0300

    att

---
+++ ThreadTasks/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread tarefa = new Thread(Tarefa);
+            tarefa.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Principal");
+            }
+
+
+            Console.ReadKey();
+        }
+
+        static void Tarefa()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Tarefa executada");
+            }
+        }
+    }
+}

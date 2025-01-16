f99ff93d1ada9030041b32914f3e8cfb551c1953
Guilherme-gt <ggk0830755@gmail.com>
2025-01-16 17:32:48 -0300

    att

--- UnimakeDFE/UNIMAKEDFE/Form1.Designer.cs
+++ UnimakeDFE/UNIMAKEDFE/Form1.Designer.cs
@@ -29,18 +29,20 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.BtnConsultaStatus = new System.Windows.Forms.Button();
-            this.BtnConsultaServico = new System.Windows.Forms.Button();
             this.BtnEnvioSincrono = new System.Windows.Forms.Button();
+            this.BtnConsultaSituacao = new System.Windows.Forms.Button();
+            this.BtnConsultaStatus = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.nightControlBox1 = new ReaLTaiizor.Controls.NightControlBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.BtnEnvioSincrono);
-            this.groupBox1.Controls.Add(this.BtnConsultaServico);
+            this.groupBox1.Controls.Add(this.BtnConsultaSituacao);
             this.groupBox1.Controls.Add(this.BtnConsultaStatus);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(11, 9);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(195, 435);
@@ -48,43 +50,83 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Nfe";
             // 
-            // BtnConsultaStatus
-            // 
-            this.BtnConsultaStatus.Location = new System.Drawing.Point(10, 34);
-            this.BtnConsultaStatus.Name = "BtnConsultaStatus";
-            this.BtnConsultaStatus.Size = new System.Drawing.Size(178, 31);
-            this.BtnConsultaStatus.TabIndex = 0;
-            this.BtnConsultaStatus.Text = "Consulta Status";
-            this.BtnConsultaStatus.UseVisualStyleBackColor = true;
-            this.BtnConsultaStatus.Click += new System.EventHandler(this.BtnConsultaStatus_Click);
-            // 
-            // BtnConsultaServico
-            // 
-            this.BtnConsultaServico.Location = new System.Drawing.Point(10, 71);
-            this.BtnConsultaServico.Name = "BtnConsultaServico";
-            this.BtnConsultaServico.Size = new System.Drawing.Size(178, 31);
-            this.BtnConsultaServico.TabIndex = 1;
-            this.BtnConsultaServico.Text = "Consulta Serviço";
-            this.BtnConsultaServico.UseVisualStyleBackColor = true;
-            this.BtnConsultaServico.Click += new System.EventHandler(this.BtnConsultaServico_Click);
-            // 
             // BtnEnvioSincrono
             // 
+            this.BtnEnvioSincrono.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BtnEnvioSincrono.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnEnvioSincrono.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnEnvioSincrono.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.BtnEnvioSincrono.Location = new System.Drawing.Point(8, 202);
             this.BtnEnvioSincrono.Name = "BtnEnvioSincrono";
             this.BtnEnvioSincrono.Size = new System.Drawing.Size(178, 31);
             this.BtnEnvioSincrono.TabIndex = 2;
             this.BtnEnvioSincrono.Text = "EnvioNFe sincrono ";
-            this.BtnEnvioSincrono.UseVisualStyleBackColor = true;
+            this.BtnEnvioSincrono.UseVisualStyleBackColor = false;
             this.BtnEnvioSincrono.Click += new System.EventHandler(this.BtnEnvioSincrono_Click);
+            // 
+            // BtnConsultaSituacao
+            // 
+            this.BtnConsultaSituacao.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BtnConsultaSituacao.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnConsultaSituacao.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnConsultaSituacao.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.BtnConsultaSituacao.Location = new System.Drawing.Point(10, 71);
+            this.BtnConsultaSituacao.Name = "BtnConsultaSituacao";
+            this.BtnConsultaSituacao.Size = new System.Drawing.Size(178, 31);
+            this.BtnConsultaSituacao.TabIndex = 1;
+            this.BtnConsultaSituacao.Text = "Consulta Situação";
+            this.BtnConsultaSituacao.UseVisualStyleBackColor = false;
+            this.BtnConsultaSituacao.Click += new System.EventHandler(this.BtnConsultaSituacao_Click);
+            // 
+            // BtnConsultaStatus
+            // 
+            this.BtnConsultaStatus.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BtnConsultaStatus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnConsultaStatus.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnConsultaStatus.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.BtnConsultaStatus.Location = new System.Drawing.Point(10, 34);
+            this.BtnConsultaStatus.Name = "BtnConsultaStatus";
+            this.BtnConsultaStatus.Size = new System.Drawing.Size(178, 31);
+            this.BtnConsultaStatus.TabIndex = 0;
+            this.BtnConsultaStatus.Text = "Consulta Status";
+            this.BtnConsultaStatus.UseVisualStyleBackColor = false;
+            this.BtnConsultaStatus.Click += new System.EventHandler(this.BtnConsultaStatus_Click);
+            // 
+            // nightControlBox1
+            // 
+            this.nightControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.nightControlBox1.BackColor = System.Drawing.Color.Transparent;
+            this.nightControlBox1.CloseHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.nightControlBox1.CloseHoverForeColor = System.Drawing.Color.White;
+            this.nightControlBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.nightControlBox1.DefaultLocation = true;
+            this.nightControlBox1.DisableMaximizeColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(105)))), ((int)(((byte)(105)))));
+            this.nightControlBox1.DisableMinimizeColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(105)))), ((int)(((byte)(105)))));
+            this.nightControlBox1.EnableCloseColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(160)))), ((int)(((byte)(160)))));
+            this.nightControlBox1.EnableMaximizeButton = true;
+            this.nightControlBox1.EnableMaximizeColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(160)))), ((int)(((byte)(160)))));
+            this.nightControlBox1.EnableMinimizeButton = true;
+            this.nightControlBox1.EnableMinimizeColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(160)))), ((int)(((byte)(160)))));
+            this.nightControlBox1.Location = new System.Drawing.Point(661, -2);
+            this.nightControlBox1.MaximizeHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.nightControlBox1.MaximizeHoverForeColor = System.Drawing.Color.White;
+            this.nightControlBox1.MinimizeHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.nightControlBox1.MinimizeHoverForeColor = System.Drawing.Color.White;
+            this.nightControlBox1.Name = "nightControlBox1";
+            this.nightControlBox1.Size = new System.Drawing.Size(139, 31);
+            this.nightControlBox1.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nightControlBox1);
             this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -94,10 +136,11 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button BtnConsultaServico;
+        private System.Windows.Forms.Button BtnConsultaSituacao;
         private System.Windows.Forms.Button BtnConsultaStatus;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.Button BtnEnvioSincrono;
+        private ReaLTaiizor.Controls.NightControlBox nightControlBox1;
     }
 }
 
--- UnimakeDFE/UNIMAKEDFE/Form1.cs
+++ UnimakeDFE/UNIMAKEDFE/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -21,15 +22,72 @@ namespace UNIMAKEDFE
         {
             InitializeComponent();
         }
+        #region Certificado Digital
 
-        private void BtnConsultaStatus_Click(object sender, EventArgs e)
+        //Caminho do Certificado Digital
+        private static string PathCertificadoDigital { get; set; } = "C:\\Users\\30755\\Downloads\\HS BERNARDO - COMERCIO DE CARNES LTDA Senha 30163001 Val 19.11.2025 1006927767.pfx";
+        //Senha do certificado Digital
+        private static string SenhaCertificado { get; set; } = "30163001";
+
+        //Campo do Certificado Digital
+        public static X509Certificate2 CertificadoDigitalField;
+
+        public static X509Certificate2 CertificadoSelecionado
         {
-
+            get
+            {
+                if (CertificadoDigitalField == null)
+                {
+                    CertificadoDigitalField = new CertificadoDigital().CarregarCertificadoDigitalA1(PathCertificadoDigital, SenhaCertificado);
+                }
+                return CertificadoDigitalField;
+            }
+            private set => throw new Exception("não é possivel carregar certificado digital, carregado automaticamente!");
         }
 
-        private void BtnConsultaServico_Click(object sender, EventArgs e)
+        #endregion
+        private void BtnConsultaStatus_Click(object sender, EventArgs e)
         {
+            var xml = new ConsStatServ
+            {
+                Versao = "4.00",
+                CUF = UFBrasil.PR,
+                TpAmb = TipoAmbiente.Homologacao
+            };
 
+            var configuracao = new Configuracao
+            {
+                CertificadoDigital = CertificadoSelecionado,
+                TipoDFe = TipoDFe.NFe,
+                TipoEmissao = TipoEmissao.Normal
+            };
+
+            var StatusServico = new StatusServico(xml, configuracao);
+            StatusServico.Executar();
+
+            MessageBox.Show(StatusServico.Result.CStat + " - " + StatusServico.Result.XMotivo);
+        }
+
+        private void BtnConsultaSituacao_Click(object sender, EventArgs e)
+        {
+            var xml = new ConsSitNFe
+            {
+                Versao = "4.00",
+                TpAmb = TipoAmbiente.Homologacao,
+                ChNFe = "41241135547526000170550010000019261000019274"
+            };
+
+            var configuracao = new Configuracao
+            {
+                CertificadoDigital = CertificadoSelecionado,
+                TipoDFe = TipoDFe.NFe,
+                TipoEmissao = TipoEmissao.Normal
+            };
+
+            var ConsultaProtocolo = new ConsultaProtocolo(xml, configuracao);
+            ConsultaProtocolo.Executar();
+
+            MessageBox.Show(ConsultaProtocolo.Result.CStat + " - " + ConsultaProtocolo.Result.XMotivo);
         }
 
         private void BtnEnvioSincrono_Click(object sender, EventArgs e)
@@ -38,7 +96,7 @@ namespace UNIMAKEDFE
             {
                 Versao = "4.00",
                 IdLote = "0000000000000001",
-                IndSinc = SimNao.Sim,
+                IndSinc = SimNao.Nao,
                 NFe = new List<NFe>
                 {
                     new NFe
@@ -138,24 +196,183 @@ namespace UNIMAKEDFE
                                        }, Imposto = new Imposto
                                        {
                                            VTotTrib = 12.63,
-                                           ICMS = new List<ICMS>
-                                           {
-                                               new ICMS
-                                               {
-                                                   ICMSSN101 = new ICMSSN101
-                                                   {
-                                                       Orig = OrigemMercadoria.Nacional,
-                                                       PCredSN = 2.8225,
-                                                       VCredICMSSN = 2.40,
-                                                   }
-                                               }
-                                           },
+                                           //ICMS = new List<ICMS>
+                                           //{
+                                           //    new ICMS
+                                           //    {
+                                           //        ICMSSN101 = new ICMSSN101
+                                           //        {
+                                           //            Orig = OrigemMercadoria.Nacional,
+                                           //            PCredSN = 2.8225,
+                                           //            VCredICMSSN = 2.40,
+                                           //        }
+                                           //    }
+                                           //},
                                            PIS = new PIS
                                            {
+                                               PISOutr = new PISOutr
+                                               {
+                                                   CST = "",
+                                                   VBC = 0.00,
+                                                   PPIS = 0.00,
+                                                   VPIS = 0.00
+                                               }
+                                           },
+                                           COFINS = new COFINS
+                                           {
+                                               COFINSOutr = new COFINSOutr
+                                               {
+                                                   CST = "99",
+                                                   VBC = 0.00,
+                                                   PCOFINS = 0.00,
+                                                   VCOFINS = 0.00
+                                               }
+                                           }
+                                       }
 
+                                   },
+                                   new Det
+                                   {
+                                       NItem = 2,
+                                       Prod = new Prod
+                                       {
+                                           CProd = "",
+                                           CEAN = "SEM GTIN",
+                                           XProd = "",
+                                           NCM = "",
+                                           CFOP = "",
+                                           UCom = "",
+                                           QCom = 1.00m,
+                                           VUnCom = 84.9000000000m,
+                                           VProd = 84.90,
+                                           CEANTrib = "SEM GTIN",
+                                           UTrib = "",
+                                           QTrib = 1.00m,
+                                           VUnTrib = 84.90000000000m,
+                                           IndTot = SimNao.Sim,
+                                           XPed = "",
+                                           NItemPed = "",
+                                       },  Imposto = new Imposto
+                                       {
+                                           VTotTrib = 12.63,
+                                           //ICMS = new List<ICMS>
+                                           //{
+                                           //    new ICMS
+                                           //    {
+                                           //        ICMSSN101 = new ICMSSN101
+                                           //        {
+                                           //            Orig = OrigemMercadoria.Nacional,
+                                           //            PCredSN = 2.8225,
+                                           //            VCredICMSSN = 2.40,
+                                           //        }
+                                           //    }
+                                           //},
+                                           PIS = new PIS
+                                           {
+                                               PISOutr = new PISOutr
+                                               {
+                                                   CST = "",
+                                                   VBC = 0.00,
+                                                   PPIS = 0.00,
+                                                   VPIS = 0.00
+                                               }
+                                           },
+                                           COFINS = new COFINS
+                                           {
+                                               COFINSOutr = new COFINSOutr
+                                               {
+                                                   CST = "99",
+                                                   VBC = 0.00,
+                                                   PCOFINS = 0.00,
+                                                   VCOFINS = 0.00
+                                               }
                                            }
                                        }
                                    }
+                               },
+                               Total = new Total
+                               {
+                                   ICMSTot = new ICMSTot
+                                   {
+                                       VBC = 0,
+                                       VICMS = 0,
+                                       VICMSDeson = 0,
+                                       VFCP = 0,
+                                       VBCST = 0,
+                                       VST = 0,
+                                       VFCPST = 0,
+                                       VFCPSTRet = 0,
+                                       VProd = 169.80,
+                                       VFrete =0,
+                                       VSeg = 0,
+                                       VDesc = 0,
+                                       VII = 0,
+                                       VIPI = 0,
+                                       VIPIDevol = 0,
+                                       VPIS = 0,
+                                       VCOFINS = 0,
+                                       VOutro = 0,
+                                       VNF = 0,
+                                       VTotTrib = 25.26
+                                   }
+                               },
+                               Transp = new Transp
+                               {
+                                   ModFrete = ModalidadeFrete.ContratacaoFretePorContaRemetente_CIF,
+                                   Vol = new List<Vol>
+                                   {
+                                       new Vol
+                                       {
+                                           QVol = 1,
+                                           Esp = "LU",
+                                           Marca = "UNIMAKE",
+                                           PesoL = 0.000,
+                                           PesoB = 0.000
+                                       }
+                                   }
+                               },
+                               Cobr = new Cobr()
+                               {
+                                   Fat = new Fat
+                                   {
+                                       NFat = "",
+                                       VOrig = 169.80,
+                                       VDesc = 0,
+                                       VLiq = 169.80
+                                   },
+                                   Dup = new List<Dup>
+                                   {
+                                       new Dup
+                                       {
+                                           NDup = "001",
+                                           DVenc = DateTime.Now,
+                                           VDup = 169.80
+
+                                       }
+                                   }
+                               },
+                               Pag = new Pag
+                               {
+                                   DetPag = new List<DetPag>
+                                   {
+                                       new DetPag
+                                       {
+                                           IndPag = IndicadorPagamento.PagamentoVista,
+                                           TPag = MeioPagamento.Dinheiro,
+                                           VPag = 169.80
+                                       }
+                                   }
+                               },
+                               InfAdic = new InfAdic
+                               {
+                                   InfCpl = ";CONTROLE: ; PEDIDO(S) ATENDIDO(S): ; Empresa optante pelo simples nacional, conforme lei compl. 128 de 19/12"
+                               },
+                               InfRespTec = new InfRespTec
+                               {
+                                   CNPJ = "",
+                                   XContato = "",
+                                   Email = "",
+                                   Fone = ""
                                }
 
                            }

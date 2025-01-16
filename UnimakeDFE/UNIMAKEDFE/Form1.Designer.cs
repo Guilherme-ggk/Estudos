f2d90225c03ebbfa2bd09cfd678aa3b8508e2725
Guilherme-gt <ggk0830755@gmail.com>
2025-01-16 14:57:39 -0300

    ATT

--- UnimakeDFE/UNIMAKEDFE/Form1.Designer.cs
+++ UnimakeDFE/UNIMAKEDFE/Form1.Designer.cs
@@ -28,13 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.BtnConsultaStatus = new System.Windows.Forms.Button();
+            this.BtnConsultaServico = new System.Windows.Forms.Button();
+            this.BtnEnvioSincrono = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BtnEnvioSincrono);
+            this.groupBox1.Controls.Add(this.BtnConsultaServico);
+            this.groupBox1.Controls.Add(this.BtnConsultaStatus);
+            this.groupBox1.Location = new System.Drawing.Point(11, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(195, 435);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nfe";
+            // 
+            // BtnConsultaStatus
+            // 
+            this.BtnConsultaStatus.Location = new System.Drawing.Point(10, 34);
+            this.BtnConsultaStatus.Name = "BtnConsultaStatus";
+            this.BtnConsultaStatus.Size = new System.Drawing.Size(178, 31);
+            this.BtnConsultaStatus.TabIndex = 0;
+            this.BtnConsultaStatus.Text = "Consulta Status";
+            this.BtnConsultaStatus.UseVisualStyleBackColor = true;
+            this.BtnConsultaStatus.Click += new System.EventHandler(this.BtnConsultaStatus_Click);
+            // 
+            // BtnConsultaServico
+            // 
+            this.BtnConsultaServico.Location = new System.Drawing.Point(10, 71);
+            this.BtnConsultaServico.Name = "BtnConsultaServico";
+            this.BtnConsultaServico.Size = new System.Drawing.Size(178, 31);
+            this.BtnConsultaServico.TabIndex = 1;
+            this.BtnConsultaServico.Text = "Consulta Serviço";
+            this.BtnConsultaServico.UseVisualStyleBackColor = true;
+            this.BtnConsultaServico.Click += new System.EventHandler(this.BtnConsultaServico_Click);
+            // 
+            // BtnEnvioSincrono
+            // 
+            this.BtnEnvioSincrono.Location = new System.Drawing.Point(8, 202);
+            this.BtnEnvioSincrono.Name = "BtnEnvioSincrono";
+            this.BtnEnvioSincrono.Size = new System.Drawing.Size(178, 31);
+            this.BtnEnvioSincrono.TabIndex = 2;
+            this.BtnEnvioSincrono.Text = "EnvioNFe sincrono ";
+            this.BtnEnvioSincrono.UseVisualStyleBackColor = true;
+            this.BtnEnvioSincrono.Click += new System.EventHandler(this.BtnEnvioSincrono_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button BtnConsultaServico;
+        private System.Windows.Forms.Button BtnConsultaStatus;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button BtnEnvioSincrono;
     }
 }
 
--- UnimakeDFE/UNIMAKEDFE/Form1.cs
+++ UnimakeDFE/UNIMAKEDFE/Form1.cs
@@ -7,6 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Unimake.Business.DFe.Servicos;
+using Unimake.Business.DFe.Servicos.NFe;
+using Unimake.Business.DFe.Xml.NFe;
+using Unimake.Security.Platform;
+
 
 namespace UNIMAKEDFE
 {
@@ -15,6 +20,150 @@ namespace UNIMAKEDFE
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void BtnConsultaStatus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnConsultaServico_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnEnvioSincrono_Click(object sender, EventArgs e)
+        {
+            var xml = new EnviNFe
+            {
+                Versao = "4.00",
+                IdLote = "0000000000000001",
+                IndSinc = SimNao.Sim,
+                NFe = new List<NFe>
+                {
+                    new NFe
+                    {
+                        InfNFe = new List<InfNFe>
+                        {
+                           new InfNFe
+                           {
+                               Versao = "4.00",
+                               Ide = new Ide
+                               {
+                                   CUF = UFBrasil.PR,
+                                   NatOp = "VENDA",
+                                   Mod = ModeloDFe.NFe,
+                                   Serie = 1,
+                                   NNF = 57972,
+                                   DhEmi = DateTime.Now,
+                                   DhSaiEnt = DateTime.Now,
+                                   TpNF = TipoOperacao.Saida,
+                                   IdDest = DestinoOperacao.OperacaoInterestadual,
+                                   CMunFG = 4118402,
+                                   TpImp = FormatoImpressaoDANFE.NormalRetrato,
+                                   TpEmis = TipoEmissao.Normal,
+                                   TpAmb = TipoAmbiente.Homologacao,
+                                   FinNFe = FinalidadeNFe.Normal,
+                                   IndFinal = SimNao.Sim,
+                                   IndPres = IndicadorPresenca.OperacaoPresencial,
+                                   ProcEmi = ProcessoEmissao.AplicativoContribuinte,
+                                   VerProc = "TESTE 1.00"
+                               },
+                               Emit = new Emit
+                               {
+                                   CNPJ = "",
+                                   XNome = "",
+                                   XFant = "",
+                                   EnderEmit = new EnderEmit
+                                   {
+                                       XLgr = "",
+                                       Nro = "",
+                                       XBairro = "",
+                                       CMun = 4118402,
+                                       XMun = "",
+                                       UF = UFBrasil.PR,
+                                       CEP = "",
+                                       Fone = ""
+
+                                   },
+                                   IE = "",
+                                   IM = "",
+                                   CNAE = "",
+                                   CRT = CRT.SimplesNacional
+
+                               },
+                               Dest = new Dest
+                               {
+                                   CNPJ = "",
+                                   XNome = "",
+                                   EnderDest = new EnderDest
+                                   {
+                                       XLgr = "",
+                                       Nro = "",
+                                       XBairro = "",
+                                       CMun = 1,
+                                       XMun = "",
+                                       UF = UFBrasil.PR,
+                                       CEP = "",
+                                       Fone = ""
+                                   },
+                                   IndIEDest = IndicadorIEDestinatario.ContribuinteICMS,
+                                   IE = "",
+                                   Email = ""
+                               },
+                               Det = new List<Det>
+                               {
+                                   new Det
+                                   {
+                                       NItem = 1,
+                                       Prod = new Prod
+                                       {
+                                           CProd = "",
+                                           CEAN = "SEM GTIN",
+                                           XProd = "",
+                                           NCM = "",
+                                           CFOP = "",
+                                           UCom = "",
+                                           QCom = 1.00m,
+                                           VUnCom = 84.9000000000m,
+                                           VProd = 84.90,
+                                           CEANTrib = "SEM GTIN",
+                                           UTrib = "",
+                                           QTrib = 1.00m,
+                                           VUnTrib = 84.90000000000m,
+                                           IndTot = SimNao.Sim,
+                                           XPed = "",
+                                           NItemPed = "",
+
+                                       }, Imposto = new Imposto
+                                       {
+                                           VTotTrib = 12.63,
+                                           ICMS = new List<ICMS>
+                                           {
+                                               new ICMS
+                                               {
+                                                   ICMSSN101 = new ICMSSN101
+                                                   {
+                                                       Orig = OrigemMercadoria.Nacional,
+                                                       PCredSN = 2.8225,
+                                                       VCredICMSSN = 2.40,
+                                                   }
+                                               }
+                                           },
+                                           PIS = new PIS
+                                           {
+
+                                           }
+                                       }
+                                   }
+                               }
+
+                           }
+                        }
+                    }
+                }
+
+            };
         }
     }
 }

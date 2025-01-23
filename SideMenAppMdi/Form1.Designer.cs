7b3063933185bf5783458e6bc2651abfde0b4bad
Gui-gt <ggk0830755@gmail.com>
2025-01-22 22:54:15 -0300

    ATT

---
+++ SideMenAppMdi/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace SideMenAppMdi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanelPrincipal = new System.Windows.Forms.Panel();
+            this.BtnHome = new System.Windows.Forms.Button();
+            this.BtnConfig = new System.Windows.Forms.Button();
+            this.BtnSair = new System.Windows.Forms.Button();
+            this.PanelForm = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PanelPrincipal.SuspendLayout();
+            this.PanelForm.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PanelPrincipal
+            // 
+            this.PanelPrincipal.BackColor = System.Drawing.Color.Orange;
+            this.PanelPrincipal.Controls.Add(this.BtnSair);
+            this.PanelPrincipal.Controls.Add(this.BtnConfig);
+            this.PanelPrincipal.Controls.Add(this.BtnHome);
+            this.PanelPrincipal.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PanelPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.PanelPrincipal.Name = "PanelPrincipal";
+            this.PanelPrincipal.Size = new System.Drawing.Size(200, 450);
+            this.PanelPrincipal.TabIndex = 0;
+            // 
+            // BtnHome
+            // 
+            this.BtnHome.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BtnHome.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnHome.FlatAppearance.BorderSize = 0;
+            this.BtnHome.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.BtnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnHome.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnHome.Location = new System.Drawing.Point(0, 0);
+            this.BtnHome.Name = "BtnHome";
+            this.BtnHome.Size = new System.Drawing.Size(200, 38);
+            this.BtnHome.TabIndex = 0;
+            this.BtnHome.Text = "Home";
+            this.BtnHome.UseVisualStyleBackColor = false;
+            this.BtnHome.Click += new System.EventHandler(this.BtnHome_Click);
+            // 
+            // BtnConfig
+            // 
+            this.BtnConfig.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BtnConfig.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnConfig.FlatAppearance.BorderSize = 0;
+            this.BtnConfig.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.BtnConfig.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnConfig.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnConfig.Location = new System.Drawing.Point(0, 38);
+            this.BtnConfig.Name = "BtnConfig";
+            this.BtnConfig.Size = new System.Drawing.Size(200, 38);
+            this.BtnConfig.TabIndex = 1;
+            this.BtnConfig.Text = "Config";
+            this.BtnConfig.UseVisualStyleBackColor = false;
+            this.BtnConfig.Click += new System.EventHandler(this.BtnConfig_Click);
+            // 
+            // BtnSair
+            // 
+            this.BtnSair.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BtnSair.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BtnSair.FlatAppearance.BorderSize = 0;
+            this.BtnSair.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BtnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnSair.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSair.Location = new System.Drawing.Point(0, 412);
+            this.BtnSair.Name = "BtnSair";
+            this.BtnSair.Size = new System.Drawing.Size(200, 38);
+            this.BtnSair.TabIndex = 2;
+            this.BtnSair.Text = "Sair";
+            this.BtnSair.UseVisualStyleBackColor = false;
+            // 
+            // PanelForm
+            // 
+            this.PanelForm.Controls.Add(this.label1);
+            this.PanelForm.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PanelForm.Location = new System.Drawing.Point(200, 0);
+            this.PanelForm.Name = "PanelForm";
+            this.PanelForm.Size = new System.Drawing.Size(600, 450);
+            this.PanelForm.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Home Principal";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PanelForm);
+            this.Controls.Add(this.PanelPrincipal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.PanelPrincipal.ResumeLayout(false);
+            this.PanelForm.ResumeLayout(false);
+            this.PanelForm.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelPrincipal;
+        private System.Windows.Forms.Button BtnSair;
+        private System.Windows.Forms.Button BtnConfig;
+        private System.Windows.Forms.Button BtnHome;
+        private System.Windows.Forms.Panel PanelForm;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SideMenAppMdi/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SideMenAppMdi
+{
+    public partial class Form1 : Form
+    {
+        private Form FrmAtivo;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FormShow(Form Frm)
+        {
+            ButtonClose();
+            FrmAtivo = Frm;
+            Frm.TopLevel = false;
+            PanelForm.Controls.Add(Frm);
+            Frm.BringToFront();
+            Frm.Show();
+        }
+        
+        private void ButtonClose()
+        {
+            if(FrmAtivo != null)
+                FrmAtivo.Close();
+        }
+
+        private void ButtonActive(Button FrmAtivo)
+        {
+            foreach(Control controle in PanelPrincipal.Controls)
+                controle.ForeColor = Color.Black;
+
+                FrmAtivo.ForeColor = Color.Blue;
+
+        }
+
+        private void BtnHome_Click(object sender, EventArgs e)
+        {
+            ButtonActive(BtnHome);
+            ButtonClose();
+        }
+
+        private void BtnConfig_Click(object sender, EventArgs e)
+        {
+            ButtonActive(BtnConfig);
+            FormShow(new FrmConfig());
+        }
+    }
+}

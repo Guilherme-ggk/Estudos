4634c739a8acfc67e7ae58d4e1483c2b4f3bea80
Guilherme <ggk0830755@gmail.com>
2025-06-18 14:47:45 -0300

    att

--- BarraLateralSuaveC#/BarraLateralSuaveC#/Form1.Designer.cs
+++ BarraLateralSuaveC#/BarraLateralSuaveC#/Form1.Designer.cs
@@ -59,11 +59,12 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(85, 9);
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(62, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(220, 20);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "Coding Ideas | Modern SideBar";
             // 
             // Form1
             // 

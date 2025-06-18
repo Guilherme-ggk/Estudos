d824b7a444663eb1cf78054a56da9ea95b7d1efb
Guilherme <ggk0830755@gmail.com>
2025-06-18 15:07:45 -0300

    att

--- BarraLateralC#/Form1.Designer.cs
+++ BarraLateralC#/Form1.Designer.cs
@@ -29,30 +29,32 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.BtnDashBoard = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.BtnAbout = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.BtnLogout = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.BtnSettings = new System.Windows.Forms.Button();
+            this.nightControlBox1 = new ReaLTaiizor.Controls.NightControlBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(756, 415);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(32, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "x";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.nightControlBox1);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
@@ -80,29 +82,167 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Coding Ideas | Modern SideBar";
             // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.flowLayoutPanel1.Controls.Add(this.panel2);
+            this.flowLayoutPanel1.Controls.Add(this.panel3);
+            this.flowLayoutPanel1.Controls.Add(this.panel5);
+            this.flowLayoutPanel1.Controls.Add(this.panel4);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 39);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(184, 411);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.BtnDashBoard);
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(180, 61);
+            this.panel2.TabIndex = 3;
+            // 
+            // BtnDashBoard
+            // 
+            this.BtnDashBoard.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BtnDashBoard.FlatAppearance.BorderSize = 0;
+            this.BtnDashBoard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnDashBoard.ForeColor = System.Drawing.Color.White;
+            this.BtnDashBoard.Location = new System.Drawing.Point(23, 12);
+            this.BtnDashBoard.Name = "BtnDashBoard";
+            this.BtnDashBoard.Size = new System.Drawing.Size(133, 37);
+            this.BtnDashBoard.TabIndex = 0;
+            this.BtnDashBoard.Text = "     DashBoard";
+            this.BtnDashBoard.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnDashBoard.UseVisualStyleBackColor = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.BtnAbout);
+            this.panel3.Location = new System.Drawing.Point(3, 70);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(180, 61);
+            this.panel3.TabIndex = 4;
+            // 
+            // BtnAbout
+            // 
+            this.BtnAbout.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BtnAbout.FlatAppearance.BorderSize = 0;
+            this.BtnAbout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnAbout.ForeColor = System.Drawing.Color.White;
+            this.BtnAbout.Location = new System.Drawing.Point(23, 12);
+            this.BtnAbout.Name = "BtnAbout";
+            this.BtnAbout.Size = new System.Drawing.Size(133, 37);
+            this.BtnAbout.TabIndex = 0;
+            this.BtnAbout.Text = "     About";
+            this.BtnAbout.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnAbout.UseVisualStyleBackColor = false;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.BtnLogout);
+            this.panel4.Location = new System.Drawing.Point(3, 204);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(180, 61);
+            this.panel4.TabIndex = 5;
+            // 
+            // BtnLogout
+            // 
+            this.BtnLogout.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BtnLogout.FlatAppearance.BorderSize = 0;
+            this.BtnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnLogout.ForeColor = System.Drawing.Color.White;
+            this.BtnLogout.Location = new System.Drawing.Point(23, 12);
+            this.BtnLogout.Name = "BtnLogout";
+            this.BtnLogout.Size = new System.Drawing.Size(133, 37);
+            this.BtnLogout.TabIndex = 0;
+            this.BtnLogout.Text = "     Logout";
+            this.BtnLogout.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnLogout.UseVisualStyleBackColor = false;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.BtnSettings);
+            this.panel5.Location = new System.Drawing.Point(3, 137);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(180, 61);
+            this.panel5.TabIndex = 6;
+            // 
+            // BtnSettings
+            // 
+            this.BtnSettings.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BtnSettings.FlatAppearance.BorderSize = 0;
+            this.BtnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnSettings.ForeColor = System.Drawing.Color.White;
+            this.BtnSettings.Location = new System.Drawing.Point(23, 12);
+            this.BtnSettings.Name = "BtnSettings";
+            this.BtnSettings.Size = new System.Drawing.Size(133, 37);
+            this.BtnSettings.TabIndex = 0;
+            this.BtnSettings.Text = "     Settings";
+            this.BtnSettings.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnSettings.UseVisualStyleBackColor = false;
+            // 
+            // nightControlBox1
+            // 
+            this.nightControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.nightControlBox1.BackColor = System.Drawing.Color.Transparent;
+            this.nightControlBox1.CloseHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.nightControlBox1.CloseHoverForeColor = System.Drawing.Color.White;
+            this.nightControlBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.nightControlBox1.DefaultLocation = true;
+            this.nightControlBox1.DisableMaximizeColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(105)))), ((int)(((byte)(105)))));
+            this.nightControlBox1.DisableMinimizeColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(105)))), ((int)(((byte)(105)))));
+            this.nightControlBox1.EnableCloseColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(160)))), ((int)(((byte)(160)))));
+            this.nightControlBox1.EnableMaximizeButton = true;
+            this.nightControlBox1.EnableMaximizeColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(160)))), ((int)(((byte)(160)))));
+            this.nightControlBox1.EnableMinimizeButton = true;
+            this.nightControlBox1.EnableMinimizeColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(160)))), ((int)(((byte)(160)))));
+            this.nightControlBox1.Location = new System.Drawing.Point(661, 0);
+            this.nightControlBox1.MaximizeHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.nightControlBox1.MaximizeHoverForeColor = System.Drawing.Color.White;
+            this.nightControlBox1.MinimizeHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.nightControlBox1.MinimizeHoverForeColor = System.Drawing.Color.White;
+            this.nightControlBox1.Name = "nightControlBox1";
+            this.nightControlBox1.Size = new System.Drawing.Size(139, 31);
+            this.nightControlBox1.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button BtnDashBoard;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button BtnAbout;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button BtnSettings;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button BtnLogout;
+        private ReaLTaiizor.Controls.NightControlBox nightControlBox1;
     }
 }
 

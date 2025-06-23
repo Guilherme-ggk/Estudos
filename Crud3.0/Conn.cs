d7ced48c85038e3e10f87fe18e4620734644920f
Guilherme <ggk0830755@gmail.com>
2025-06-23 14:02:12 -0300

    att

---
+++ Crud3.0/Conn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crud3._0
+{
+    static class Conn
+    {
+        private const string Servidor = "localhost";
+        private const string BancoDados = "livros";
+        private const string usuario = "root";
+        private const string Senha = "0830755@Guilherme";
+
+        static public string StrConn = $"server={Servidor};database={BancoDados};user id={usuario};password={Senha}";
+    }
+}

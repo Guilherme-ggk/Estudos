bc7a136636aeab79c1fa413b2c8962d37d17d7d3
Guilherme <ggk0830755@gmail.com>
2025-09-16 11:28:53 -0300

    refazendo

--- CrudSetembro/Form1.cs
+++ CrudSetembro/Form1.cs
@@ -16,49 +16,5 @@ namespace CrudSetembro
         {
             InitializeComponent();
         }
-
-        private void BtnExit_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void BtnData_Click(object sender, EventArgs e)
-        {
-           
-        }
-
-        private void BtnHome_Click(object sender, EventArgs e)
-        {
-          
-        }
-
-
-        Form FrmAtivo;
-
-        private void FormShow(Form Frm)
-        {
-            FormClose();
-            FrmAtivo = Frm;
-            Frm.TopLevel = false;
-            PanelForm.Controls.Add(Frm);
-            Frm.BringToFront();
-            Frm.Show();
-        }
-
-        private void FormClose()
-        {
-            if(FrmAtivo != null)
-            {
-                FrmAtivo.Close();
-            }
-        }
-
-        private void ButtonActive(Button FrmAtivo)
-        {
-            foreach(Control Ctrl in PanelPrincipal.Controls)
-                Ctrl.ForeColor = Color.Black;
-
-            FrmAtivo.ForeColor = Color.Blue;
-        }
     }
 }

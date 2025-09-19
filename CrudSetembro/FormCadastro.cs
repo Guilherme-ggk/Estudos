03c23676489b93b99d4a87f90290b84306d092a1
Guilherme <ggk0830755@gmail.com>
2025-09-18 23:51:11 -0300

    att

--- CrudSetembro/FormCadastro.cs
+++ CrudSetembro/FormCadastro.cs
@@ -12,9 +12,32 @@ namespace CrudSetembro
 {
     public partial class FormCadastro : Form
     {
-        public FormCadastro()
+        int id;
+        Livros livro = new Livros();
+        public FormCadastro(int id)
         {
             InitializeComponent();
+            this.id = id;
+          
+
+            if(this.id > 0)
+            {
+
+                livro.GetLivro(this.id);
+
+                LblId.Text = livro.Id.ToString();
+                TxtIsbn.Text = livro.Isbn;
+                TxtTitulo.Text = livro.Titulo;
+                TxtAutores.Text = livro.Autores;
+                TxtUnitario.Text = livro.Unitario.ToString("n2");
+                TxtSaldo.Text = livro.Saldo_inicial.ToString();
+                TxtEstoque.Text = livro.Estoque_minimo.ToString();
+                livro.Ativo = 'S';
+                ChkAtivo.Checked = true;
+
+
+
+            }
         }
     }
 }

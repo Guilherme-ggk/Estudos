5961fd983011f0e1532e0678cea498b65a7d7090
Guilherme <ggk0830755@gmail.com>
2025-09-20 11:35:21 -0300

    att

--- CrudSetembro/FormCadastro.Designer.cs
+++ CrudSetembro/FormCadastro.Designer.cs
@@ -114,7 +114,7 @@
             // 
             // BtnExcluir
             // 
-            this.BtnExcluir.Location = new System.Drawing.Point(259, 151);
+            this.BtnExcluir.Location = new System.Drawing.Point(214, 151);
             this.BtnExcluir.Name = "BtnExcluir";
             this.BtnExcluir.Size = new System.Drawing.Size(58, 24);
             this.BtnExcluir.TabIndex = 8;

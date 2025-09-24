d891a41ff0b2192eb32d629192e8129a41e88dc3
Guilherme <ggk0830755@gmail.com>
2025-09-23 22:51:18 -0300

    att

--- CrudSetembro/Form1.Designer.cs
+++ CrudSetembro/Form1.Designer.cs
@@ -125,7 +125,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(81, 89);
+            this.button2.Location = new System.Drawing.Point(26, 81);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(91, 29);
             this.button2.TabIndex = 4;

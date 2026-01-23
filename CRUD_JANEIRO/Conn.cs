609a115bad125f435eba3dec51bbe75285377977
Guilherme <ggk0830755@gmail.com>
2026-01-23 11:07:33 -0300

    att

---
+++ CRUD_JANEIRO/Conn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_JANEIRO
+{
+    static class Conn
+    {
+        private const string Servidor = "localhost";
+        private const string BancoDados = "livros";
+        private const string Usuario = "root";
+        private const string Senha = "0830755@Guilherme";
+
+
+        public static string StrConn = $"server={Servidor};database={BancoDados};userid={Usuario};password={Senha}";
+    }
+}
---
+++ CRUD_JANEIRO/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace CRUD_JANEIRO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DgvLivros = new System.Windows.Forms.DataGridView();
+            this.BtnAdicionar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvLivros)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DgvLivros
+            // 
+            this.DgvLivros.AllowUserToAddRows = false;
+            this.DgvLivros.AllowUserToDeleteRows = false;
+            this.DgvLivros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvLivros.Location = new System.Drawing.Point(124, 117);
+            this.DgvLivros.Name = "DgvLivros";
+            this.DgvLivros.ReadOnly = true;
+            this.DgvLivros.Size = new System.Drawing.Size(520, 303);
+            this.DgvLivros.TabIndex = 0;
+            // 
+            // BtnAdicionar
+            // 
+            this.BtnAdicionar.Location = new System.Drawing.Point(12, 12);
+            this.BtnAdicionar.Name = "BtnAdicionar";
+            this.BtnAdicionar.Size = new System.Drawing.Size(97, 42);
+            this.BtnAdicionar.TabIndex = 1;
+            this.BtnAdicionar.Text = "adicionar";
+            this.BtnAdicionar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnAdicionar);
+            this.Controls.Add(this.DgvLivros);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.DgvLivros)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DgvLivros;
+        private System.Windows.Forms.Button BtnAdicionar;
+    }
+}

155fbad2a84487d39452e2a128013fc6f6cdfa7b
Guilherme <ggk0830755@gmail.com>
2026-02-18 22:56:45 -0300

    att

--- Python/script.js
+++ Python/script.js
@@ -1,4 +1,21 @@
 
 
 
-let frutas = ["","","","abacaxi"];
+let frutas = ["uva","maçã","melão","abacaxi"];
+
+for(fruta of frutas)
+{
+    console.log(frutas[0]);
+}
+
+
+let pessoa = {
+    nome : "Guilherme Henrique",
+    idade : 22,
+    cidade : "Rolândia"
+}
+
+for (chave in pessoa)
+{
+    console.log(pessoa[chave]);
+}

6f39f3fdf320de7b99a6b9f5fa6d9d6cdb175346
Guilherme <ggk0830755@gmail.com>
2026-02-22 20:13:37 -0300

    att

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 1;
+            int n2 = 2;
+
+            int soma = n1 + n2;
+
+            Console.WriteLine(soma);
+
+            Console.ReadLine();
+        }
+    }
+}

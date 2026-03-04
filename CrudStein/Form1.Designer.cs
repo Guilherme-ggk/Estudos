16545847042a51e63091dd52814525c39daafe6d
Guilherme <ggk0830755@gmail.com>
2026-03-03 22:14:46 -0300

    att

---
+++ CrudStein/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace CrudStein
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanelPrincipal = new System.Windows.Forms.Panel();
+            this.PanelForm = new System.Windows.Forms.Panel();
+            this.PanelPicture = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.BtnHome = new System.Windows.Forms.Button();
+            this.PanelPrincipal.SuspendLayout();
+            this.PanelPicture.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PanelPrincipal
+            // 
+            this.PanelPrincipal.BackColor = System.Drawing.Color.White;
+            this.PanelPrincipal.Controls.Add(this.BtnHome);
+            this.PanelPrincipal.Controls.Add(this.PanelPicture);
+            this.PanelPrincipal.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PanelPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.PanelPrincipal.Name = "PanelPrincipal";
+            this.PanelPrincipal.Size = new System.Drawing.Size(172, 450);
+            this.PanelPrincipal.TabIndex = 0;
+            // 
+            // PanelForm
+            // 
+            this.PanelForm.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PanelForm.Location = new System.Drawing.Point(172, 0);
+            this.PanelForm.Name = "PanelForm";
+            this.PanelForm.Size = new System.Drawing.Size(628, 450);
+            this.PanelForm.TabIndex = 1;
+            // 
+            // PanelPicture
+            // 
+            this.PanelPicture.Controls.Add(this.pictureBox1);
+            this.PanelPicture.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanelPicture.Location = new System.Drawing.Point(0, 0);
+            this.PanelPicture.Name = "PanelPicture";
+            this.PanelPicture.Size = new System.Drawing.Size(172, 47);
+            this.PanelPicture.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(37, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(75, 29);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BtnHome
+            // 
+            this.BtnHome.BackColor = System.Drawing.Color.White;
+            this.BtnHome.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnHome.FlatAppearance.BorderSize = 0;
+            this.BtnHome.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.BtnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnHome.Location = new System.Drawing.Point(0, 47);
+            this.BtnHome.Name = "BtnHome";
+            this.BtnHome.Size = new System.Drawing.Size(172, 39);
+            this.BtnHome.TabIndex = 1;
+            this.BtnHome.Text = "HOME";
+            this.BtnHome.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PanelForm);
+            this.Controls.Add(this.PanelPrincipal);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.PanelPrincipal.ResumeLayout(false);
+            this.PanelPicture.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelPrincipal;
+        private System.Windows.Forms.Panel PanelPicture;
+        private System.Windows.Forms.Panel PanelForm;
+        private System.Windows.Forms.Button BtnHome;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+

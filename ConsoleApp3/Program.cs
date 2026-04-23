e484ef22534207aa3d19a662896845fb5ff31410
Guilherme <ggk0830755@gmail.com>
2026-04-22 23:02:10 -0300

    att

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -9,30 +9,74 @@ namespace ConsoleApp3
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("< Aumento de Crédito >");
+            Console.Write("Digite seu CPF:");
 
-            string nome = Console.ReadLine();
-            string cpf = Console.ReadLine();
-            int rendaAtual = int.Parse(Console.ReadLine());
+            string CPF = Console.ReadLine();
 
-            bool limiteUsuario = false;
-            
+            bool valido = ValidaCPF(CPF);
 
-            if(string.IsNullOrEmpty(nome) || string.IsNullOrEmpty(cpf) || rendaAtual == 0)
+            if(valido)
             {
-                Console.WriteLine("Dados incompletos!");
+                Console.WriteLine("CPF válido!");
             }
             else
             {
-
+                Console.WriteLine("CPF inválido!");
             }
-            
+        }
 
+        static bool ValidaCPF(string cpf)
+        {
+            cpf = cpf.Replace(".", "").Replace("-", "");
 
+            if(cpf.Length != 11)
+                return false;
 
-            
+            if(TodosIguais(cpf))
+                return false;
+
+            string baseCpf = cpf.Substring(0, 9);
+
+            int digito1 = CalculaDigito(baseCpf, 10);
+            int digito2 = CalculaDigito(baseCpf + digito1, 11);
+
+            string cpfGerado = baseCpf + digito1.ToString() + digito2.ToString();
+
+            return cpf == cpfGerado;
+        }
+
+        static int CalculaDigito(string cpf, int pesoInicial)
+        {
+            int soma = 0;
+
+            for(int i = 0; i < cpf.Length; i++)
+            {
+                int numero = int.Parse(cpf[i].ToString());
+                soma += numero * (pesoInicial - i);
+            }
+
+            int resto = soma % 11;
+
+            if(resto < 2)
+            {
+                return 0;
+            }
+            else
+            {
+                return 11 - resto;
+            }
+        }
+
+        static bool TodosIguais(string cpf)
+        {
+            for(int i = 0; i < cpf.Length; i++)
+            {
+                if (cpf[i] != cpf[0])
+                    return false;
+            }
+            return true;
         }
     }
 }

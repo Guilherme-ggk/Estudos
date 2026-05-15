90e91040cadde2b5b724ce400698b1f1d3f5d9ea
Guilherme <ggk0830755@gmail.com>
2026-05-15 16:24:02 -0300

    att

--- Console C# - Curso Victor Lima/POO/POO/Program.cs
+++ Console C# - Curso Victor Lima/POO/POO/Program.cs
@@ -8,8 +8,42 @@ namespace POO
 {
     internal class Program
     {
+        // atributos de um jogo: nome, tipo, tamanho, preço
+        // métodos para um jogo: abrir, carregar, executar, atualizar
+
+
+
+        public class Jogo
+        {
+            public string Nome { get; set; }
+            public string Genero { get; set; }
+            public string Preco { get; set; }
+
+
+            static void Atualizar()
+            {
+
+            }
+            static void Abrir()
+            {
+
+            }
+            static void Executar()
+            {
+
+            }
+            static void Carregar()
+            {
+
+            }
+        }
+
+
+
         static void Main(string[] args)
         {
+           Jogo jogo = new Jogo();
+            jogo.
         }
     }
 }

3a6e090bfdbabed1b9ecf338c713f92f8245dfde
Guilherme <ggk0830755@gmail.com>
2026-05-19 16:46:04 -0300

    att

--- Console C# - Curso Victor Lima/POO/POO/Program.cs
+++ Console C# - Curso Victor Lima/POO/POO/Program.cs
@@ -1,23 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace POO
 {
-    internal class Program
+    class Program
     {
         // atributos de um jogo: nome, tipo, tamanho, preço
         // métodos para um jogo: abrir, carregar, executar, atualizar
 
+        #region exemplo de classe jogo
 
-
-        public class Jogo
+        static class Jogo
         {
-            public string Nome { get; set; }
-            public string Genero { get; set; }
-            public string Preco { get; set; }
+            static public string Nome { get; set; }
+            static string Genero { get; set; }          // Fortnite - BattleRoyale - R$: 0
+            static string Preco { get; set; }
 
 
             static void Atualizar()
@@ -38,12 +39,63 @@ namespace POO
             }
         }
 
+        #endregion
 
 
-        static void Main(string[] args)
+        #region Exemplos de abstração em classes
+
+        class Celular
         {
-           Jogo jogo = new Jogo();
-            jogo.
+            public string Nome { get; set; }
+            public string Marca { get; set; }
+            public int Preco { get; set; }
+
+            public List<string> ListaAcessorios = new List<string>();
+        }
+
+        class Jogo
+        {
+            public string Nome { get; set; }
+            public string Genero { get; set; }
+            public int Preco { get; set; }
+            public string Desenvolvedora { get; set; }
+            public string Plataforma { get; set; }
+            public int TamanhoDownload { get; set; }
+        }
+
+        class PedidoRestaurante
+        {
+            public string Prato { get; set; }
+            public string Bebida { get; set; }
+            public int Preco { get; set; }
+            public int Comanda { get; set; }
+
+            public List<string> listaDeRefeicoes = new List<string>();
+            public int ValorTotal { get; set; }
+        }
+
+        class PersonagemRPG
+        {
+            public string nome { get; set; }
+            public int Nivel { get; set; }
+            public int PontoVida { get; set; }
+            public int PontoPoder { get; set; }
+
+            public List<string> itens = new List<string>();
+        }
+
+        class ImovelAluguel
+        {
+            public string Endereco { get; set; }
+            public int ValorAluguel { get; set; }
+            public int CEP { get; set; }
+        }
+
+        #endregion
+
+        static void Main()
+        {
+            
         }
     }
 }
--- Console C# - Curso Victor Lima/POO/TESTE/Program.cs
+++ Console C# - Curso Victor Lima/POO/TESTE/Program.cs
@@ -6,10 +6,63 @@ using System.Threading.Tasks;
 
 namespace TESTE
 {
+    public class Jogos
+    {
+        public string Nome { get; set; }
+        public int Preco { get; set; }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome} | Preço:{Preco:C}";
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            List<Jogos> jogos = new List<Jogos>();
+
+            Jogos jogo = new Jogos();
+
+            jogos.Add(new Jogos { Nome = "teste", Preco = 10 });
+            jogos.Add(new Jogos { Nome = "teste1", Preco = 20});
+
+            Console.Write("digite o novo jogo: ");
+             jogo.Nome = Console.ReadLine();
+             jogo.Preco = int.Parse(Console.ReadLine());
+
+            jogos.Add(jogo);
+
+            //Console.WriteLine(string.Join($"{jogos[0].Nome}"));
+
+
+            foreach (Jogos jogoG in jogos)
+            {
+                Console.WriteLine(jogoG);
+            }
+
+            bool existeJogo = jogos.Any(x => x.Nome == "teste");
+
+            Console.WriteLine(existeJogo);
+
+
+
+            Console.WriteLine("----------------------------------------");
+
+           List<int> numeros = new List<int>();
+
+            numeros.Add(1);
+            numeros.Add(1);
+            numeros.Add(1);
+            numeros.Add(1);
+
+            int somador = numeros.Sum();
+
+            Console.WriteLine(somador);
+
+            Console.ReadLine();
+
         }
     }
 }

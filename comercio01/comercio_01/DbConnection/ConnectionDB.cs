09a9bd013c7ff2120942a7345f495980b2c6a683
Guilherme <ggk0830755@gmail.com>
2026-08-21 23:50:14 -0300

    att

---
+++ comercio01/comercio_01/DbConnection/ConnectionDB.cs
@@ -0,0 +1,7 @@
+﻿namespace comercio_01.DbConnection
+{
+     public class ConnectionDB
+    {
+        static public  string conn = "server=localhost;database=farmacia;userid=root;password=0830755@Guilherme;";
+    }
+}

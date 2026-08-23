726fe25c3b3fe94948a8c2a04d5bc20e64b66026
Guilherme <ggk0830755@gmail.com>
2026-08-22 21:06:06 -0300

    att

--- comercio01/comercio_01/Program.cs
+++ comercio01/comercio_01/Program.cs
@@ -1,4 +1,3 @@
-
 using comercio_01.Models;
 using comercio_01.Repository;
 
@@ -13,18 +12,29 @@ namespace comercio_01
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+            // CORS - permite o Angular acessar a API
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AngularPolicy", policy =>
+                {
+                    policy
+                        .WithOrigins("http://localhost:4200")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
             builder.Services.AddScoped<CategoriaRepository>();
             builder.Services.AddScoped<FornecedorRepository>();
             builder.Services.AddScoped<ProdutoRepository>();
             builder.Services.AddScoped<FuncionarioRepository>();
-            builder.Services.AddScoped<ProdutoRepository>();
             builder.Services.AddScoped<VendaRepository>();
             builder.Services.AddScoped<Item_VendaRepository>();
             builder.Services.AddScoped<ClienteRepository>();
-            
 
             var app = builder.Build();
 
@@ -37,8 +47,10 @@ namespace comercio_01
 
             app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+            // CORS
+            app.UseCors("AngularPolicy");
 
+            app.UseAuthorization();
 
             app.MapControllers();
 
@@ -46,3 +58,54 @@ namespace comercio_01
         }
     }
 }
+//using comercio_01.Models;
+//using comercio_01.Repository;
+
+//namespace comercio_01
+//{
+//    public class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            var builder = WebApplication.CreateBuilder(args);
+
+//            // Add services to the container.
+
+//            builder.Services.AddControllers();
+//            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//            builder.Services.AddEndpointsApiExplorer();
+//            builder.Services.AddSwaggerGen();
+//            builder.Services.AddScoped<CategoriaRepository>();
+//            builder.Services.AddScoped<FornecedorRepository>();
+//            builder.Services.AddScoped<ProdutoRepository>();
+//            builder.Services.AddScoped<FuncionarioRepository>();
+//            builder.Services.AddScoped<ProdutoRepository>();
+//            builder.Services.AddScoped<VendaRepository>();
+//            builder.Services.AddScoped<Item_VendaRepository>();
+//            builder.Services.AddScoped<ClienteRepository>();
+
+
+//            var app = builder.Build();
+
+//            // Configure the HTTP request pipeline.
+//            if (app.Environment.IsDevelopment())
+//            {
+//                app.UseSwagger();
+//                app.UseSwaggerUI();
+//            }
+
+
+
+//            app.UseHttpsRedirection();
+
+//            app.UseAuthorization();
+
+
+//            app.MapControllers();
+
+
+
+//            app.Run();
+//        }
+//    }
+//}
--- comercio01/comercio_01/obj/Debug/net8.0/comercio_01.AssemblyInfo.cs
+++ comercio01/comercio_01/obj/Debug/net8.0/comercio_01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("comercio_01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+540e8f90fed78e4576fa93be48a4084350879c1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("comercio_01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("comercio_01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
